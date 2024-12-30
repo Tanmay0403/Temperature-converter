e957fc0ce01761d61505b8a3508a98aa1fcb052d
Tanmay Chachad <tanmaychachad@gmail.com>
2024-12-30 12:31:06 +0530

    Uploading Files

---
+++ script.js
@@ -0,0 +1,41 @@
+var input = document.getElementById("temperature");
+var from = document.getElementById("from");
+var to = document.getElementById("to")
+var output = document.querySelector(".result");
+var unit = document.querySelector(".unit");
+
+
+from.addEventListener('change',function(){
+    if(from.value === 'C')
+    {
+        to.value = 'F';
+        to.options[1].disabled = true;
+    }
+    else if(from.value === 'F')
+    {
+        to.value = 'C';
+        to.options[0].disabled = true;
+    }
+})
+
+function convert()
+{
+    var temperature = parseFloat(input.value);
+    if(from.value === 'C'&& to.value === 'F')
+    {
+        var result = temperature * 9/5 + 32;
+        output.innerText = result;
+        unit.innerHTML = "<sup>o</sup> F";
+    }
+    else if(from.value === 'F' && to.value === 'C')
+    {
+        var result = (temperature - 32) / (9/5);
+        output.innerText = result;
+        unit.innerHTML = "<sup>o</sup> C";
+    }
+    else{
+        alert("Something went wrong");
+    }
+    
+}
+
